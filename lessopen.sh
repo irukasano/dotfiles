b6d28ce9c335a665e4b43afb8ca17d081002248b
sano <sano@iruka-system.co.jp>
2020-03-24 12:14:07 +0900

    fixes lang=ja_JP.UTF8

--- lessopen.sh
+++ lessopen.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
+export LANG="ja_JP.UTF8"
+
 case "$1" in
   *.md)
-    pandoc -s -f markdown -t man "$1" | groff -T utf8 -man > /tmp/less.$$ 2>/dev/null
+    pandoc -s -f markdown -t man "$1" | man -l > /tmp/less.$$ 2>/dev/null
+    #pandoc -s -f markdown -t man "$1" | groff -T utf8 -man > /tmp/less.$$ 2>/dev/null
     if [ -s /tmp/less.$$ ]; then
         echo /tmp/less.$$
     else
